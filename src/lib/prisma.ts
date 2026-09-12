25a57c250de8333cdc3ce1b42e65a50c77ad33d9
saleh <saleh.barzegar11@gmail.com>
2026-09-12 09:57:41 +0330

    version 2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,21 +8,18 @@ const globalForPrisma = globalThis as unknown as {
 const adapter = new PrismaMariaDb({
   host: "127.0.0.1",
   port: 3306,
-  user: "erpramyco",
-  password: "@Sb.1385@11",
-  database: "erpramyco_db",
-  connectionLimit: 30
+  user: "root",
+  password: "",
+  database: "erpramyco",
 });
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-    log: ["error"], // در زمان بیلد بهتر است فقط error ها لاگ شوند تا سرعت بالا برود
+    log: ["error"],
   });
 
 if (!globalForPrisma.prisma) {
   globalForPrisma.prisma = prisma;
-}// auto deploy test
-// auto deploy final test
-
+}
